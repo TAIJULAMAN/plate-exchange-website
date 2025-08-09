580a54afb958828721369c5a0103c5e5ff5a31c1
Parves Mosarof <parvesmosarof2@gmail.com>
2025-08-10 05:25:41 +0600

    added how it works

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Banner() {
     return (
-        <div className="bg-green-600 text-white py-3">
+        <div className="bg-[#00823A] text-white py-6 w-full font-sans">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="text-center">
-                    <p className="text-sm md:text-base font-medium">
+                    <p className="text-xl font-semibold">
                         Why pay more? Save up to 30% with direct plate deals.
                     </p>
                 </div>
