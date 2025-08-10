e11beab0287c24212da7b55ee4666543c61afac2
TAIJULAMAN <taijulaman8@gmail.com>
2025-08-11 00:54:27 +0600

    changed SellAPlateHero

--- src/pages/SellAPlate/SellAPlate.jsx
+++ src/pages/SellAPlate/SellAPlate.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import NewlyListedPlates from '../Home/NewlyListedPlates/NewlyListedPlates'
+import PricingPlans from '../Home/PricingPlans/PricingPlans'
+import HelpfulGuides from '../Home/HelpfulGuides/HelpfulGuides'
+import FAQSection from '../Home/FAQSection/FAQSection'
+import Review from '../Home/Review/Review'
+import SellAPlateHero from './SellAPlateHero'
 export default function SellAPlate() {
   return (
-    <div>SellAPlate</div>
+    <div>
+      <SellAPlateHero />
+      <Review />
+      <NewlyListedPlates />
+      <PricingPlans />
+      <HelpfulGuides />
+      <FAQSection />
+    </div>
   )
 }
