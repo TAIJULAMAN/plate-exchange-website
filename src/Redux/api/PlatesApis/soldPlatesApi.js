caf7cd679a4c8582366e5fa41b8e73788628d88c
Parves Mosarof <parvesmosarof614@gmail.com>
2025-08-27 00:20:24 +0600

    asdf

--- src/Redux/api/PlatesApis/soldPlatesApi.js
+++ src/Redux/api/PlatesApis/soldPlatesApi.js
@@ -1,18 +1,15 @@
 import { baseApi } from "../baseApi";
 
-
 const soldPlatesApi = baseApi.injectEndpoints({
-          endpoints: (builder) => ({
-                    getSoldPlates: builder.query({
-                              query: ({params}) => ({
-                                        url: 'plates_sales/find_by_all_sold_plate_sales_list',
-                                        method: 'GET',
-                              }),
-                              providesTags: ['soldPlates'],
-                    }),
-          }),
+  endpoints: (builder) => ({
+    getSoldPlates: builder.query({
+      query: ({ page, limit }) => ({
+        url: `plates_sales/find_by_all_sold_plate_sales_list?page=${page}&limit=${limit}`,
+        method: "GET",
+      }),
+      providesTags: ["soldPlates"],
+    }),
+  }),
 });
 
-export const {
-      useGetSoldPlatesQuery,
-} = soldPlatesApi;
+export const { useGetSoldPlatesQuery } = soldPlatesApi;
